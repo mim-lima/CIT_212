305da3ad4b6e8623009143e660928f7d118f1a6c
Most.Mim <mostmimmim7@gmail.com>
2026-06-11 00:05:19 +0600

    Add files via upload

---
+++ lab2B_treetraversal.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *left, *right;
+
+    Node(int val)
+    {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+void preorder(Node *root)
+{
+    if (root == NULL) return;
+
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void inorder(Node *root)
+{
+    if (root == NULL) return;
+
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+void postorder(Node *root)
+{
+    if (root == NULL) return;
+
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
+int main()
+{
+    Node *root = new Node(1);
+
+    root->left = new Node(12);
+    root->right = new Node(9);
+
+    root->left->left = new Node(5);
+    root->left->right = new Node(6);
+
+    cout << "Preorder: ";
+    preorder(root);
+
+    cout << "\nInorder: ";
+    inorder(root);
+
+    cout << "\nPostorder: ";
+    postorder(root);
+
+    return 0;
+}
